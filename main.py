6fea6bdf84c987a1b11fbcf51a099a6f12fec311
Jeff Casavant <jeff.casavant@gmail.com>
2023-06-26 19:42:27 -0400

    Adjustments

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import requests
+import argparse
 from bs4 import BeautifulSoup
 import re
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", "--number", required=True)
+parser.add_argument("-r", "--reveal", action="store_true", help="If passed, reveal answers when skipping")
+args = parser.parse_args()
+
 # Send a GET request to the webpage
-base_url = 'https://www.mentalfloss.com/posts/ken-jennings-kennections-quiz-46'
+base_url = f"https://www.mentalfloss.com/posts/ken-jennings-kennections-quiz-{args.number}"
 answers_url = f"{base_url}/2"
 kennection_url = f"{base_url}/3"
 
 response = requests.get(answers_url)
 
 # Create a BeautifulSoup object to parse the HTML content
-soup = BeautifulSoup(response.content, 'html.parser')
+soup = BeautifulSoup(response.content, "html.parser")
 
 # Find all the paragraph elements containing the quiz questions
-question_elements = soup.find_all('p')
+question_elements = soup.find_all("p")
 
 # Use regular expressions to extract the questions and answers
-question_pattern = re.compile(r'^\d+\.\s+(.*)')  # Matches the numbering pattern followed by the question text
+question_pattern = re.compile(r"^\d+\.\s+(.*)")  # Matches the numbering pattern followed by the question text
 questions = []
 answers = []
 is_question = True  # Flag to track if a paragraph element is a question or an answer
@@ -36,9 +42,9 @@ for element in question_elements:
 
 # retrieve kennection
 response_final = requests.get(kennection_url)
-soup_final = BeautifulSoup(response_final.content, 'html.parser')
+soup_final = BeautifulSoup(response_final.content, "html.parser")
 
-answer_element = soup_final.find('p').find('strong')
+answer_element = soup_final.find("p").find("strong")
 final_answer = answer_element.text.strip()
 
 questions.append("What is the KENNECTION?")
@@ -58,8 +64,11 @@ for index, (question, answer) in enumerate(zip(questions, answers), start=1):
             print("Correct!")
             score += 1
             break
-        elif user_answer == r'\q':
-            print("Answer:", answer)
+        elif user_answer in [r"\q", ""]:
+            if args.reveal:
+                print("Answer:", answer)
+            else:
+                print("Skipped")
             break
         else:
             print("Incorrect. Try again.")
